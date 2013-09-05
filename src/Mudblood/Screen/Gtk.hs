931123502561077ae2bf7da7c2090c1e93eb93e2
Philip Kranz <philip.kranz@googlemail.com>
2013-09-05 21:05:25 +0200

    Screen/Gtk: Fix rendering of table widget.

--- src/Mudblood/Screen/Gtk.hs
+++ src/Mudblood/Screen/Gtk.hs
@@ -472,7 +472,7 @@ renderTableWidget style tab = do
     C.selectFontFace "monospace" C.FontSlantNormal C.FontWeightNormal
     C.setFontSize 12.0
 
-    forM_ (transpose $ rectify "" (maximum $ map length tab) tab) $ \col -> do
+    forM_ (transpose $ rectify " " (maximum $ map length tab) tab) $ \col -> do
         C.save
         widths <- forM col $ \cell -> do
             C.save
@@ -485,8 +485,7 @@ renderTableWidget style tab = do
         C.restore
         C.translate (maximum widths + 5.0) 0.0
 
-    ext <- C.textExtents "A"
-    return $ (fromIntegral $ length tab) * (C.textExtentsHeight ext)
+    return $ (fromIntegral $ length tab) * 12.0
   where
     rectify elem len mat = map (fill elem len) mat
     fill elem len l = l ++ (take (max 0 (len - length l)) $ repeat elem)
