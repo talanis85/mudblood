4f4460cb5dc06608c00d1b337a6f9923b249993e
Philip Kranz <philip.kranz@googlemail.com>
2014-02-03 11:27:17 +0100

    Added Mudblood.Contrib.Logfile
    This module provides a trigger that maintains a logfile.

---
+++ src/Mudblood/Contrib/Logfile.hs
@@ -0,0 +1,18 @@
+module Mudblood.Contrib.Logfile
+    ( logfileTrigger
+    ) where
+
+import System.IO
+
+import Mudblood
+
+logfileTrigger :: String -> MBTriggerFlow u
+logfileTrigger filename = Volatile $ \ev -> do
+    fileHandle <- io $ openFile filename AppendMode
+    logfileLoop fileHandle ev
+  where
+    logfileLoop h = keep (handleEvent h) >=> yieldT >=> logfileLoop h
+    handleEvent h ev = case ev of
+        LineTEvent x -> io $ hPutStrLn h (fromAttrString x) >> hFlush h
+        SendTEvent x -> io $ hPutStrLn h ("> " ++ x) >> hFlush h
+        _ -> return ()
