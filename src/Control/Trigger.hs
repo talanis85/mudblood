82dc90e730a745a851ef2a38a4158e27e8da8905
Philip Kranz <philip.kranz@googlemail.com>
2014-02-04 21:19:16 +0100

    Control.Trigger: Introduce stateful triggers.

--- src/Control/Trigger.hs
+++ src/Control/Trigger.hs
@@ -1,7 +1,18 @@
 module Control.Trigger
     ( module Control.Trigger.Monad
     , module Control.Trigger.Flow
+
+    , lift, get, modify
+    , statefulT
     ) where
 
 import Control.Trigger.Monad
 import Control.Trigger.Flow
+
+import Control.Monad.State
+import Control.Monad.Trans
+
+-- | Evaluate a state transformed trigger. The StateT is looped infinitely.
+statefulT :: (Monad m) => s -> (a -> StateT s (TriggerM m y a) y) -> (a -> TriggerM m y a o)
+statefulT initialState trigger = \x -> evalStateT (loop x) initialState
+    where loop = trigger >=> lift . yieldT >=> loop
